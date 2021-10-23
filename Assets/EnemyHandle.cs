288e47ce919b20d7d9b9ad647fd9dd1bcd3c085e
Simon <simongonand@gmail.com>
2021-10-23 20:34:22 +0200

    Enemies are pursuing player

--- Assets/EnemyHandle.cs
+++ Assets/EnemyHandle.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHandle : MonoBehaviour
 {
+    public Transform selfHips;
     public Animator selfAnimator;
+    public NavMeshAgent selfNavMesh;
     public ParticleSystem bloodDropFX;
 
     private bool isDie;
+    private Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
         isDie = false;
+        player = PlayerController.instance.self;
     }
 
     public void Die()
@@ -20,6 +25,7 @@ public class EnemyHandle : MonoBehaviour
         selfAnimator.SetBool("IsDead", true);
         bloodDropFX.Play();
         isDie = true;
+        --EnemyManager.instance.nbEnemies;
     }
 
     private void IsDieAnimationFinished()
@@ -33,6 +39,8 @@ public class EnemyHandle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!isDie)
+            selfNavMesh.SetDestination(player.position);
         IsDieAnimationFinished();
     }
 }
